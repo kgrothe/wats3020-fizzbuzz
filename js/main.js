fbd3eef5595168ae32a63856d3bdbd605cec8a71
kgrothe <kboyle@seattleu.edu>
2018-04-22 05:58:06 +0000

    Revised main.js.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* Code for WATS 3020 FizzBuzz Assignment */
 
-let isInteger,
+var isInteger,
     maxNumber,
     fbResults,
     fbText;
@@ -9,11 +9,18 @@ let isInteger,
 // TODO: Initialize a variable `isInteger` to use as a control value for the
 // `while` loop. Set the initial value to `false`.
 
+isInteger = false;
+
 // TODO: Create a `while` loop so user will be prompted to enter a number until
 // they enter a good number. This loop should be controlled by a conditional
 // expression that looks at the value of `isSafeInteger`. When `isSafeInteger`
 // becomes `true`, the `while` loop should stop looping.
-
+while (!isInteger) { //run while isInteger = false
+    maxNumber = prompt('Please enter a whole number greater than 0.', 20); // default 20
+    maxNumber = parseInt(maxNumber, 10);//convert string to an integer
+    if (Number.isSafeInteger(maxNumber) && maxNumber > 0)    //check if number is safe integer and is greater than 0, if yes, change isInteger to true
+    isInteger = true;  
+}
 // TODO: Inside `while` loop prompt the user for the `maxNumber` value.
 
 // TODO: Inside the `while` loop, use a conditional to verify if the `maxNumber`
@@ -24,7 +31,26 @@ let isInteger,
 
 // TODO: Initialize the `fbResults` variable to an empty Array
 
+fbResults = [];
+
 // TODO: Create a `for` loop that will execute the `maxNumber` of times.
+
+
+for (let i=1; i<= maxNumber; i++){
+        if (i % 15 === 0){
+        fbResults.push('fizzbuzz');
+    }
+    else if (i % 3 === 0) {
+        fbResults.push('fizz');        
+    }
+    else if (i % 5 === 0) {
+        fbResults.push('buzz');
+    }
+    else fbResults.push(i); 
+}
+
+
+
 
 // TODO: Create logic inside the `for` loop to calculate FizzBuzz.  This will
 // require the use of several conditional statements that use the `%` operator.
@@ -33,9 +59,13 @@ let isInteger,
 // TODO: In preparation for creating the output text for your FizzBuzz results,
 // initialize the `fbText` variable to an empty string.
 
+fbText = "";
+
 // TODO: Use a `for ... of` loop to concatenate the values from `fbResults`
 // into the variable `fbText`.
-
+for (let result of fbResults) {
+    fbText = fbText + result + "\n";
+}
 
 ///////////////////////////////////////////////////////////////////////
 // Do not edit below this line unless you are doing something fancy!
