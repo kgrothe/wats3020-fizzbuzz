03ab6b5e3dc6eb4c4194eeee8b22c808c0c3d85d
kgrothe <kboyle@seattleu.edu>
2018-04-22 22:12:27 +0000

    Revised main.js.

--- js/main.js
+++ js/main.js
@@ -5,36 +5,21 @@ var isInteger,
     fbResults,
     fbText;
 
-
-// TODO: Initialize a variable `isInteger` to use as a control value for the
-// `while` loop. Set the initial value to `false`.
-
 isInteger = false;
 
-// TODO: Create a `while` loop so user will be prompted to enter a number until
-// they enter a good number. This loop should be controlled by a conditional
-// expression that looks at the value of `isSafeInteger`. When `isSafeInteger`
-// becomes `true`, the `while` loop should stop looping.
-while (!isInteger) { //run while isInteger = false
-    maxNumber = prompt('Please enter a whole number greater than 0.', 20); // default 20
-    maxNumber = parseInt(maxNumber, 10);//convert string to an integer
-    if (Number.isSafeInteger(maxNumber) && maxNumber > 0)    //check if number is safe integer and is greater than 0, if yes, change isInteger to true
-    isInteger = true;  
+while (isInteger === false){
+    
+    maxNumber = prompt('Please enter a whole number greater than 0:');   
+           
+    maxNumber = parseInt(maxNumber, 10);//convert string to an integer      
+    
+    if (Number.isSafeInteger(maxNumber) && maxNumber > 0)    //check if number is safe integer and is greater than 0, if yes change isInteger to true
+    isInteger = true;       
+    
+    else alert ('Sorry, that is not a whole number. Please try again.');
 }
-// TODO: Inside `while` loop prompt the user for the `maxNumber` value.
-
-// TODO: Inside the `while` loop, use a conditional to verify if the `maxNumber`
-// is a suitable integer. (It should be an integer greater than zero.)
-
-// TODO: If the value of `maxNumber` is suitable, then change the value of
-// `isInteger` so the `while` loop stops looping.
-
-// TODO: Initialize the `fbResults` variable to an empty Array
 
 fbResults = [];
-
-// TODO: Create a `for` loop that will execute the `maxNumber` of times.
-
 
 for (let i=1; i<= maxNumber; i++){
         if ((i % 5 === 0) && (i % 3 ===0)){
@@ -49,20 +34,9 @@ for (let i=1; i<= maxNumber; i++){
     else fbResults.push(i); 
 }
 
-
-
-
-// TODO: Create logic inside the `for` loop to calculate FizzBuzz.  This will
-// require the use of several conditional statements that use the `%` operator.
-// Store the results of this logic in an array called `fbResults`.
-
-// TODO: In preparation for creating the output text for your FizzBuzz results,
-// initialize the `fbText` variable to an empty string.
-
 fbText = "";
 
-// TODO: Use a `for ... of` loop to concatenate the values from `fbResults`
-// into the variable `fbText`.
+
 for (let result of fbResults) {
     fbText = fbText + result + "\n";
 }
