b6f497b44e8b9e72db4b814bf0b5d99b409a7b75
kgrothe <kboyle@seattleu.edu>
2018-04-23 05:08:33 +0000

    Revised main.js and index.html.

--- js/main.js
+++ js/main.js
@@ -7,28 +7,28 @@ var isInteger,
 
 isInteger = false;
 
-while (isInteger === false){
+while (isInteger == false){
     
-    maxNumber = prompt('Please enter a whole number greater than 0:');   
-           
-    maxNumber = parseInt(maxNumber, 10);//convert string to an integer      
+    maxNumber = prompt('Please enter a whole number greater than 0:');  
     
-    if (Number.isSafeInteger(maxNumber) && maxNumber > 0)    //check if number is safe integer and is greater than 0, if yes change isInteger to true
-    isInteger = true;       
+    maxNumber = parseFloat(maxNumber); //convert string to number
     
-    else alert ('Sorry, that is not a whole number. Please try again.');
+    if (Number.isSafeInteger(maxNumber, 10) && maxNumber > 0) //check if whole number AND greater than 0
+    isInteger = true;               
+        
+    else alert ('Sorry, that is not a valid number. Please try again.');
 }
 
 fbResults = [];
 
 for (let i=1; i<= maxNumber; i++){
-        if ((i % 5 === 0) && (i % 3 ===0)){
+        if ((i % 5 == 0) && (i % 3 ==0)){
         fbResults.push('FizzBuzz');
     }
-    else if (i % 3 === 0) {
+    else if (i % 3 == 0) {
         fbResults.push('Fizz');        
     }
-    else if (i % 5 === 0) {
+    else if (i % 5 == 0) {
         fbResults.push('Buzz');
     }
     else fbResults.push(i); 
@@ -37,6 +37,7 @@ for (let i=1; i<= maxNumber; i++){
 fbText = "";
 
 for (let result of fbResults) {
+       
     fbText = fbText + result + "\n";
 }
 
